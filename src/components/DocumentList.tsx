6d6f33da69874c1d23f5836b08fc69b4b726409d
shaswat-FE <shaswat.p1692@gmail.com>
2026-04-29 02:24:49 +0530

    fix for add button

--- src/components/DocumentList.tsx
+++ src/components/DocumentList.tsx
@@ -2,6 +2,7 @@ import {
   Table, TableHead, TableRow, TableCell,
   TableBody, Button
 } from "@mui/material";
+import { addLineItems } from "src/api/documentApi";
 
 export default function DocumentList({ documents, refresh, showToast }: any) {
   const handleAdd = async (ref: string) => {
